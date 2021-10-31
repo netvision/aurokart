aa7693558028e90dd0901c4657e982ea02aab41e
Rakesh Jangid <rakesh@jangid.co.in>
2021-10-31 22:26:44 +0530

    quasar basic init && few clean up

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# aurokart
-Book Store of Sri Aurobindo Divine Life Trust
+# Aurokart (aurokart)
+
+Publication Division of SADLT
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Aurokart
+        </q-toolbar-title>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      side="right"
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer elevated>
+        <q-toolbar>
+          <q-toolbar-title><div>Quasar v{{ $q.version }}</div></q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+  </q-layout>
+</template>
+
+<script>
+
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
